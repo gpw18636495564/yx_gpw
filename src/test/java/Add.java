a9b16792626ccec0a361f8c6539d662990ecc75d
gpw18636495564 <75253849+gpw18636495564@users.noreply.github.com>
2020-12-01 20:14:19 +0800

    gpw
    删除了

--- src/test/java/Add.java
+++
@@ -1,8 +0,0 @@
-public class Add {
-    public static void main(String[] args) {
-        System.out.println("我第一次用IDEA操作git");
-        String a = "春蚕致死！蜡炬成灰";
-        System.out.println("早死早成生！希望在来生");
-        int a=10;
-    }
-}
